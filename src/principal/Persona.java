8303a0cd5b07567851c956fe9c394fb8b6f81375
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-17 02:08:27 -0500

    Implementar la clase Persona con atributos y métodos para comparación por edad y cédula.

--- src/principal/Persona.java
+++ src/principal/Persona.java
@@ -1,11 +1,49 @@
 package principal;
 
 public class Persona implements Comparable<Persona> {
+    private String nombre;
+    private String cedula;
+    private int edad;
+
+    public Persona(String nombre, String cedula, int edad) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
 
     @Override
     public int compareTo(Persona p) {
+        if (this.edad < p.edad) {
+            return -1; // retorna -1 cuando la edad actual es menor que la otra
+        }
 
-        return 0;
+        if (this.edad > p.edad) {
+            return 1; // retorna 1 cuando la edad actual es mayor que la otra
+        }
+
+        // con Strings utilizamos compareTo propio para las condicionales y comparar
+
+        if (this.cedula.compareTo(p.cedula) < 0) {
+            return -1; // la cedula va antes
+        }
+
+        if (this.cedula.compareTo(p.cedula) > 0) {
+            return 1; // la cedula va despues
+        }
+
+        return 0; // retorna 0 cuando los dos son iguales osea la misma persona
     }
 
 }
