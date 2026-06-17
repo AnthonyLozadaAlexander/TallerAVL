2239e80f894ec943d3abad80e7a74c2a37079573
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-17 02:29:37 -0500

    Agregar medición de tiempo de ejecución y mejorar la estructura del método main en la clase Main

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -6,7 +6,9 @@ import tad.TADVector;
 
 public class Main {
 	public static void main(String[] args) {
-		ArbolAVL arbol = new ArbolAVL<>("Arbol Taller AVL");
+		long inicio = System.nanoTime();
+
+		ArbolAVL<Persona> arbol = new ArbolAVL<>("Arbol Taller AVL");
 		String[] nombres = { "Peter", "Donald", "Hugo", "Paco", "Luis", "Gohan", "Goku", "Piccolo", "Satan", "Krilin" };
 		TADVector<Persona> p = new TADVector<>(10, "Vector");
 
@@ -14,6 +16,17 @@ public class Main {
 			p.insertarElemento(new Persona(nombres[i], Algoritmos.cedulaAleatorio(), Algoritmos.aleatorio(18, 50)), i);
 			arbol.insertar(p.leerElemento(i));
 		}
+		System.out.println("\n");
+
+		arbol.info();
+
+		int index = p.buscarElemento2(new Persona("Gohan", "", 0));
+		Persona pBuscar = p.leerElemento(index);
+
+		arbol.buscarNodo(pBuscar);
+
+		long fin = System.nanoTime();
+		System.out.println("Tiempo Total Ejecucion: " + (fin - inicio) + " [ns]");
 	}
 
 }
