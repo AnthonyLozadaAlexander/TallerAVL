3e237a86fbed683f9fdd0c05dbbb313b90f8e43e
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-17 02:20:49 -0500

    Implementar la lógica principal en la clase Main para crear un árbol AVL y un vector de objetos Persona, insertando elementos aleatorios en el árbol.

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,8 +1,19 @@
 package principal;
 
+import algoritmos.Algoritmos;
+import arbolBinario.*;
+import tad.TADVector;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		ArbolAVL arbol = new ArbolAVL<>("Arbol Taller AVL");
+		String[] nombres = { "Peter", "Donald", "Hugo", "Paco", "Luis", "Gohan", "Goku", "Piccolo", "Satan", "Krilin" };
+		TADVector<Persona> p = new TADVector<>(10, "Vector");
+
+		for (int i = 0; i < nombres.length; i++) {
+			p.insertarElemento(new Persona(nombres[i], Algoritmos.cedulaAleatorio(), Algoritmos.aleatorio(18, 50)), i);
+			arbol.insertar(p.leerElemento(i));
+		}
 	}
 
 }
