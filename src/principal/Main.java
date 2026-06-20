59e46aaabd3250a125db521ecbb2298bbe1b508c
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-19 21:43:22 -0500

    Reorganizar la estructura del método main en la clase Main, mejorando la inicialización de los árboles ABB y AVL. Se agregó la medición del tiempo de búsqueda de nodos existentes y no existentes, y se ajustaron los mensajes de salida para mayor claridad.

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -7,32 +7,73 @@ import tad.TADVector;
 public class Main {
 	public static void main(String[] args) {
 		Long inicio = System.nanoTime();
-
-		String[] nombres = { "Peter", "Donald", "Hugo", "Paco", "Luis", "Gohan", "Goku", "Piccolo", "Satan", "Krilin" };
+		ArbolBB<Persona> abb = new ArbolBB<>("miABB");
+		ArbolAVL<Persona> avl = new ArbolAVL<>("Arbol Taller AVL");
 		TADVector<Persona> p = new TADVector<>(10, "Vector");
-		ArbolAVL<Persona> arbol = new ArbolAVL<>("Arbol Taller AVL");
+		Persona goku = new Persona("goku", "1234567890", 47);
+		p.insertarElemento(goku, 6);
 
-		for (int i = 0; i < nombres.length; i++) {
+		String[] nombres = { "Peter", "Donald", "Hugo", "Paco", "Luis", "Gohan", goku.getNombre() , "Piccolo", "Satan" };
+		
+
+		/* for (int i = 0; i < nombres.length -1 ; i++) {
 			p.insertarElemento(new Persona(nombres[i], Algoritmos.cedulaAleatorio(), Algoritmos.aleatorio(18, 50)), i);
 			arbol.insertar(p.leerElemento(i));
-		}
+		} */
+
+		System.out.println("------Informacion Del Arbol ABB------");
+		abb.insertar(new Persona(nombres[0], Algoritmos.cedulaAleatorio(), 30));
+		abb.insertar(new Persona(nombres[1], Algoritmos.cedulaAleatorio(), 50));
+		abb.insertar(new Persona(nombres[2], Algoritmos.cedulaAleatorio(),12));
+		abb.insertar(new Persona(nombres[3], Algoritmos.cedulaAleatorio(),11));
+		abb.insertar(new Persona(nombres[4], Algoritmos.cedulaAleatorio(), 28));
+		abb.insertar(new Persona(nombres[5], Algoritmos.cedulaAleatorio(), 99));
+		
+		abb.insertar(goku);
+		abb.insertar(new Persona(nombres[7], Algoritmos.cedulaAleatorio(), 65));
+		abb.insertar(new Persona(nombres[8], Algoritmos.cedulaAleatorio(), 37));
+		abb.info();
+		
 		System.out.println("\n");
-
+		
+		// p.insertarElemento(pK); // inserto el ultimo al vector
+		//arbol.insertar(pK);
+		
 		// muestra la informacion de todo el arbol
-		arbol.info();
+		// arbol.info();
 
+		System.out.println("------Informacion Del Arbol AVL------");
+		avl.insertar(new Persona(nombres[0], Algoritmos.cedulaAleatorio(), 30));
+		avl.insertar(new Persona(nombres[1], Algoritmos.cedulaAleatorio(), 50));
+		avl.insertar(new Persona(nombres[2], Algoritmos.cedulaAleatorio(),12));
+		avl.insertar(new Persona(nombres[3], Algoritmos.cedulaAleatorio(),11));
+		avl.insertar(new Persona(nombres[4], Algoritmos.cedulaAleatorio(), 28));
+		avl.insertar(new Persona(nombres[5], Algoritmos.cedulaAleatorio(), 99));
+		
+		avl.insertar(goku);
+		avl.insertar(new Persona(nombres[7], Algoritmos.cedulaAleatorio(), 65));
+		avl.insertar(new Persona(nombres[8], Algoritmos.cedulaAleatorio(), 37));
+		avl.info();
+
+		long inicioBuscar = System.nanoTime();
 		int index = 5;
 		Persona pBuscar = p.leerElemento(index);
 		System.out.println("\nBuscar Nodo Con Dato: " + pBuscar.getNombre());
-		System.out.println(arbol.buscar(pBuscar));
+		System.out.println(avl.buscar(pBuscar));
+		long finalBuscar = System.nanoTime();
 
+		long inicioBuscarNoExistente = System.nanoTime();
 		System.out.println("\nBuscar Nodo No Existente");
 		Persona persona = new Persona("Rogert", "084324564", 24);
-		System.out.println(arbol.buscar(persona));
-		arbol.eliminar(persona);
+		System.out.println(avl.buscar(persona));
+		long finalBuscarNoExistente = System.nanoTime();
+		avl.eliminar(persona);
+
+		System.out.println("Tiempo De Busqueda De Nodo Existente: " + (finalBuscar - inicioBuscar) + "[Ns]");
+		System.out.println("Tiempo De Busqueda De Nodo No Existente: " + (finalBuscarNoExistente - inicioBuscarNoExistente) + "[Ns]");
 
 		long fin = System.nanoTime();
-		System.out.println("Tiempo Total Ejecucion: " + (fin - inicio) + " [ns]");
+		System.out.println("\nTiempo Total Ejecucion Del Programa: " + (fin - inicio) + " [ns]");
 	}
 
 }
